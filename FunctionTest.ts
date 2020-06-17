18776e1caabb97cacbdf34d2b04710b3f99f5d76
jacobdewey <66138103+jacobdewey@users.noreply.github.com>
2020-06-17 15:55:35 -0400

    Update FunctionTest.ts

--- FunctionTest.ts
+++ FunctionTest.ts
@@ -1,6 +1,6 @@
 //% weight=100 color=#333333 icon="\uf238"
 namespace FunctionTests{
-//% blockId= "FunctionTest"
+//% blockId= FunctionTest block="FunctionTest"
 export function FunctionTest(d: () => void): void {
 function doSomething(d: () => void): void {
 d
