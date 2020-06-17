8c7884103d1ad56f827fdb2ee24c4a361e0de7c6
jacobdewey <66138103+jacobdewey@users.noreply.github.com>
2020-06-17 15:47:46 -0400

    Update FunctionTest.ts

--- FunctionTest.ts
+++ FunctionTest.ts
@@ -1,4 +1,5 @@
-// Add your code here
+//% weight=100 color=#333333 icon="\uf238"
+namespace FunctionTests{
 //% blockId= FunctionTest
 function FunctionTest(d: () => void) {
 function dosomething(): void{
@@ -9,4 +10,5 @@ d
 
 
 
+}
 }
